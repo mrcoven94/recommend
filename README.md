1c9a8ce047bcf6cd766a4ea8bf4734d6f3471eab
David Coven <mrcoven94@gmail.com>
2015-01-03 11:59:13 -0800

    transfer from bitbucket

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-recommend
-=========
-
-A todo list curated by my friends, built in Rails
+My very first rails app. :)
---
+++ db/migrate/20150103163209_add_completed_at_todo_items.rb
@@ -0,0 +1,5 @@
+class AddCompletedAtTodoItems < ActiveRecord::Migration
+  def change
+    add_column :todo_items, :completed_at, :datetime
+  end
+end
